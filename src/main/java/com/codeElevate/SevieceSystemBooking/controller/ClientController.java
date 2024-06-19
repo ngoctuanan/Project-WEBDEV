8fe56ada5f48915c929cb7c42a2cfcaf272b6bcf
congson-0909 <congsontncs@gmail.com>
2024-06-19 22:20:20 +0700

    Update ClientController.java

--- src/main/java/com/codeElevate/SevieceSystemBooking/controller/ClientController.java
+++ src/main/java/com/codeElevate/SevieceSystemBooking/controller/ClientController.java
@@ -3,7 +3,7 @@ package com.codeElevate.ServiceBookingSystem.controller;
 import com.codeElevate.ServiceBookingSystem.services.client.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
