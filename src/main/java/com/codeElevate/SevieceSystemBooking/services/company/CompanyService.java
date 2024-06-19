3ba45c9778058af85b8cf264814fbb426726922f
congson-0909 <congsontncs@gmail.com>
2024-06-19 22:37:19 +0700

    Update CompanyService.java

--- src/main/java/com/codeElevate/SevieceSystemBooking/services/company/CompanyService.java
+++ src/main/java/com/codeElevate/SevieceSystemBooking/services/company/CompanyService.java
@@ -2,6 +2,7 @@ package com.codeElevate.SevieceSystemBooking.services.company;
 
 import java.io.IOException;
 import java.util.List;
+import com.codeElevate.ServiceBookingSystem.dto.ReservationDTO;
 
 import com.codeElevate.SevieceSystemBooking.dto.AdDTO;
 
@@ -10,6 +11,8 @@ public interface CompanyService {
      boolean postAd(Long userId, AdDTO adDTO) throws IOException;
      
      List<AdDTO> getAllAds(Long userId);
+    
+     List<ReservationDTO> getAllAdBookings(Long companyId);
 
      AdDTO getAdById(Long adId);
     
