ff97ba36baa3411420c18aaa289354f546b44c13
congson-0909 <congsontncs@gmail.com>
2024-06-19 22:10:09 +0700

    Update company-dashboard.component.ts

--- src/app/company/pages/company-dashboard/company-dashboard.component.ts
+++ src/app/company/pages/company-dashboard/company-dashboard.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { CompanyService } from '../services/company.service';
 
 @Component({
   selector: 'app-company-dashboard',
   templateUrl: './company-dashboard.component.html',
-  styleUrls: ['./company-dashboard.component.scss']
+  styleUrls: ['./company-dashboard.component.scss'],
 })
-export class CompanyDashboardComponent {
 
+export class CompanyDashboardComponent {
+  constructor(private companyService: CompanyService) {}
+
+  ngOnInit() {
+    this.getAllAdBookings();
+  }
+
+  getAllAdBookings() {
+    this.companyService.getAllAdBookings().subscribe(res => {
+      console.log(res);
+    })
+  }
 }
