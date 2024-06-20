7b24ef3fb5fc85ca7a278614a110b96e187fcac5
congson-0909 <congsontncs@gmail.com>
2024-06-20 12:25:32 +0700

    Update AdRepository.java

--- src/main/java/com/codeElevate/SevieceSystemBooking/repository/AdRepository.java
+++ src/main/java/com/codeElevate/SevieceSystemBooking/repository/AdRepository.java
@@ -6,8 +6,10 @@ import java.util.*;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface AdRepository extends JpaRepository<Ad, Long> { 
     List<Ad>findAllByUserId(Long userId);
+   List<Ad> findAllByServiceNameContaining(String name);
 }
